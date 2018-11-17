420811026110a7e859f8edab1b093dc96bf59557
JensDeTaey <jens.detaey.y7844@student.hogent.be>
2018-11-17 16:42:30 +0100

    First version of the models

---
+++ Windows-App/Model/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_App.Model
+{
+    class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Establishment> Establishments { get; set; }
+        public string OpeningHours { get; set; }
+        public Image Picture { get; set; }
+        public List<Promotion> Promotions { get; set; }
+        public List<Event> Events { get; set; }
+        public int NumberOfSubsribers { get; set; }
+        public string LinkWebsite { get; set; }
+
+    }
+}
