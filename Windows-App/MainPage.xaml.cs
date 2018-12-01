1555a9c772fb9922d0d72d146e13e2b3094bb870
Robin <robin.debock.y7189@student.hogent.be>
2018-12-01 17:08:43 +0100

    Functional navigation Todo: show current selected NavButton

--- Windows-App/MainPage.xaml.cs
+++ Windows-App/MainPage.xaml.cs
@@ -12,16 +12,15 @@ namespace Windows_App
 
     public sealed partial class MainPage : Page
     {
-        private List<ContentControl> navigationContentControls;
+        private List<Control> navigationControls;
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            navigationContentControls = new List<ContentControl>()
+            navigationControls = new List<Control>()
             {
-                NavPageBusinessesContentControl,
-                NavPagePromotionsContentControl
+                 
             };
         }
 
@@ -31,83 +30,37 @@ namespace Windows_App
             PageSplitView.IsPaneOpen = !PageSplitView.IsPaneOpen;
         }
 
-        private void NavigationContentControl_Tapped(object sender, TappedRoutedEventArgs e)
+        private void NavPageBusinessesControl_Click(object sender, RoutedEventArgs e)
         {
-            if (!(sender is ContentControl))
-                throw new Exception("Can't update tap on NavigationItems, sender is not of type ContentControl");
+            mainFrame.Navigate(typeof(BusinessesPage));
+            NavigationContentControlActivated(sender);
+        }
 
-            //resetting all ContentControls
-            foreach (ContentControl navigationItem in navigationContentControls)
+        private void NavPagePromotionsControl_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(PromotionsPage));
+            NavigationContentControlActivated(sender);
+        }
+
+        private void NavPageEventsContentControl_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(EventsPage));
+            NavigationContentControlActivated(sender);
+        }
+
+        private void NavigationContentControlActivated(object sender)
+        {
+            //resetting all navigation Controls
+            foreach (ContentControl navigationItem in navigationControls)
             {
                 //navigationItem.IsEnabled = true;
-
-                //resetting the content of ContentControl if available
-                //Setting the background color on ContentControl does not affect content, so we need to go deeper
-                if (navigationItem.Content is StackPanel itemStackPanel)
-                {
-                    itemStackPanel.Background = default;
-                }
             }
 
             //Casting sender to ContentControl
-            ContentControl tappedControl = sender as ContentControl;
+            Control tappedControl = sender as Control;
+            tappedControl.Background = new SolidColorBrush(Color.FromArgb(20, 20, 20, 20));
 
             //tappedControl.IsEnabled = false;
-
-            //Setting the color of the content, if available
-            if (tappedControl.Content is StackPanel controlStackPanel)
-            {
-                controlStackPanel.Background = new SolidColorBrush(Color.FromArgb(20, 20, 20, 20));
-            }
-        }
-
-        #region Navigation Hover
-
-        private void NavigationContentControl_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            if (!(sender is ContentControl))
-                throw new Exception("Can't update hover on NavigationItems, sender is not of type ContentControl");
-
-            ContentControl hoveredControl = sender as ContentControl;
-
-            //Casting content to StackPanel
-            //Setting the background color on ContentControl does not affect content, so we need to go deeper
-            if (hoveredControl.Content is StackPanel controlStackPanel)
-            {
-                controlStackPanel.Background = new SolidColorBrush(Color.FromArgb(20, 20, 20, 20));
-            }
-        }
-
-        private void NavigationContentControl_PointerExited(object sender, PointerRoutedEventArgs e)
-        {
-            if (!(sender is ContentControl))
-                throw new Exception("Can't update hover on NavigationItems, sender is not of type ContentControl");
-
-            ContentControl hoveredControl = sender as ContentControl;
-
-            //Casting content to StackPanel
-            //Setting the background color on ContentControl does not affect content, so we need to go deeper
-            if (hoveredControl.Content is StackPanel controlStackPanel)
-            {
-                controlStackPanel.Background = default;
-            }
-        }
-
-        #endregion
-
-        private void NavPageBusinessesContentControl_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            mainFrame.Navigate(typeof(BusinessesPage));
-        }
-
-        private void NavPagePromotionsStackPanel_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            mainFrame.Navigate(typeof(PromotionsPage));
-        }
-
-        private void NavPageEventsStackPanel_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            mainFrame.Navigate(typeof(EventsPage));
         }
 
     }
