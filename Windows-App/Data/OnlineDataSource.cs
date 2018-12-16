d7a0d2f456a3f0d32dce0a9e8d91d63187de9a40
Casper Verswijvelt <casperverswijvelt@gmail.com>
2018-12-16 18:09:49 +0100

    Cleaned up data source classes and put everything into sections for the lulz

--- Windows-App/Data/OnlineDataSource.cs
+++ Windows-App/Data/OnlineDataSource.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Windows_App.Data;
@@ -14,47 +15,90 @@ namespace Windows_App
     class OnlineDataSource : IDataSource
     {
 
-        private string baseUrl = "http://localhost:52420/api/";
-
-        public override async Task<ObservableCollection<Business>> FetchBusinesses()
-        {
-            HttpClient client = new HttpClient();
-            var json = await client.GetStringAsync(new Uri(baseUrl + "Businesses/"));
-            return JsonConvert.DeserializeObject<ObservableCollection<Business>>(json);
+        #region Boring stuff
+        private string BaseUrl = "http://localhost:52420/api/";
+        private HttpClient _httpClient;
+        private HttpClient HttpClient {
+            get {
+                _httpClient.DefaultRequestHeaders.Authorization = token;
+                return _httpClient;
+            }
+            set {
+                this._httpClient = value;
+            }
+        }
+        private AuthenticationHeaderValue token {
+            get {
+                //TODO Hier juiste token zetten
+                return new AuthenticationHeaderValue("Bearer", "Token yeees");
+            }
         }
 
-        public override async Task<Business> FetchBusinessWithId(int id)
+        public OnlineDataSource()
         {
-            HttpClient client = new HttpClient();
-            var json = await client.GetStringAsync(new Uri(baseUrl + "Businesses/"+id));
-            return JsonConvert.DeserializeObject<Business>(json);
+            HttpClient = new HttpClient();
+        }
+        #endregion
+        
+        #region Main Pages Getters
+        public override async Task<ObservableCollection<Business>> FetchBusinesses()
+        {
+            var json = await HttpClient.GetStringAsync(new Uri(BaseUrl + "Businesses/"));
+            return JsonConvert.DeserializeObject<ObservableCollection<Business>>(json);
         }
 
         public override async Task<ObservableCollection<Promotion>> FetchPromotions()
         {
-            HttpClient client = new HttpClient();
-            var json = await client.GetStringAsync(new Uri(baseUrl + "Promotions/"));
+            var json = await HttpClient.GetStringAsync(new Uri(BaseUrl + "Promotions/"));
             return JsonConvert.DeserializeObject<ObservableCollection<Promotion>>(json);
         }
 
         public override async Task<ObservableCollection<Event>> FetchEvents()
         {
-            HttpClient client = new HttpClient();
-            var json = await client.GetStringAsync(new Uri(baseUrl + "Events/"));
+            var json = await HttpClient.GetStringAsync(new Uri(BaseUrl + "Events/"));
             return JsonConvert.DeserializeObject<ObservableCollection<Event>>(json);
         }
-
-        public override async Task<bool> AddEstablishment(int businessId,Establishment establishment)
+        public override Task<ObservableCollection<Business>> FetchSubscribedBusinesses()
         {
-            HttpClient client = new HttpClient();
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(baseUrl + "Businesses/" + businessId + "/AddEstablishment"));
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Business Actions
+        public override async Task<Business> FetchBusinessWithId(int id)
+        {
+            var json = await HttpClient.GetStringAsync(new Uri(BaseUrl + "Businesses/" + id));
+            return JsonConvert.DeserializeObject<Business>(json);
+        }
+
+        public override Task<bool> EditBusiness(Business business)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> SubscribeToBusiness(int businessId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> UnsubscribeFromBusiness(int businessId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Establishment CRUD
+        public override async Task<bool> AddEstablishment(int businessId, Establishment establishment)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(BaseUrl + "Businesses/" + businessId + "/AddEstablishment"));
             requestMessage.Content = new StringContent(JsonConvert.SerializeObject(establishment));
-            var json = await client.SendAsync(requestMessage).Result.Content.ReadAsStringAsync();
+            var json = await HttpClient.SendAsync(requestMessage).Result.Content.ReadAsStringAsync();
             try
             {
                 JsonConvert.DeserializeObject<Establishment>(json);
                 return true;
-            } catch (Exception)
+            }
+            catch (Exception)
             {
                 return false;
             }
@@ -62,10 +106,9 @@ namespace Windows_App
 
         public override async Task<bool> EditEstablishment(Establishment establishment)
         {
-            HttpClient client = new HttpClient();
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Put, new Uri(baseUrl + "Establishments/" + establishment.Id));
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Put, new Uri(BaseUrl + "Establishments/" + establishment.Id));
             requestMessage.Content = new StringContent(JsonConvert.SerializeObject(establishment));
-            var json = await client.SendAsync(requestMessage).Result.Content.ReadAsStringAsync();
+            var json = await HttpClient.SendAsync(requestMessage).Result.Content.ReadAsStringAsync();
             try
             {
                 JsonConvert.DeserializeObject<Establishment>(json);
@@ -79,9 +122,8 @@ namespace Windows_App
 
         public override async Task<bool> DeleteEstablishment(Establishment establishment)
         {
-            HttpClient client = new HttpClient();
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Delete, new Uri(baseUrl + "Establishments/" + establishment.Id));
-            var json = await client.SendAsync(requestMessage).Result.Content.ReadAsStringAsync();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Delete, new Uri(BaseUrl + "Establishments/" + establishment.Id));
+            var json = await HttpClient.SendAsync(requestMessage).Result.Content.ReadAsStringAsync();
             try
             {
                 JsonConvert.DeserializeObject<Establishment>(json);
@@ -92,5 +134,41 @@ namespace Windows_App
                 return false;
             }
         }
+        #endregion
+
+        #region Promotion CRUD
+        public override Task<bool> AddPromotion(int establishmentId, Promotion promotion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> EditPromotion(Promotion promotion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> DeletePromotion(Promotion promotion)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Event CRUD
+        public override Task<bool> AddEvent(int establishmentId, Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> EditEvent(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> DeleteEvent(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
     }
 }
