f7e62a7486d70989bc8365f8e9658443fdc1d9d3
Robin <robin.debock.y7189@student.hogent.be>
2018-12-16 13:31:16 +0100

    Authentication handler en bearer aangemaakt. AuthenticationBearer ingevuld

---
+++ Windows-App/Data/AuthenticationBearer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_App.Data
+{
+    public class AuthenticationBearer
+    {
+        public string AccessToken { get; set; }
+        private DateTime expireDate;
+        public DateTime ExpireDate
+        {
+            set { expireDate = value; }
+        }
+        //Check if expire time lies before the current time
+        public Boolean IsExpired() => DateTime.Compare(expireDate, DateTime.Now) < 0; 
+        public Boolean IsBusinessAccount { get; set; }
+    }
+}
